40a72746b6639aca979c631c1fbeca7b19e18ca4
Max Ghenis <mghenis@gmail.com>
2021-07-17 11:08:10 -0700

    Update URL in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="synthimpute",
     version="0.1",
     description="Python package for data synthesis and imputation using parametric and nonparametric methods, and evaluation of these methods.",
-    url="http://github.com/maxghenis/synthimpute",
+    url="http://github.com/PSLmodels/synthimpute",
     author="Max Ghenis",
     author_email="mghenis@gmail.com",
     license="MIT",
