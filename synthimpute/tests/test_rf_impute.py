ea14530c2524fd16e28064e5cd638d6853938c61
Max Ghenis <mghenis@gmail.com>
2021-07-17 10:56:14 -0700

    Add test for mean_quantile

--- synthimpute/tests/test_rf_impute.py
+++ synthimpute/tests/test_rf_impute.py
@@ -15,8 +15,11 @@ def test_rf_impute():
     x_train = x.iloc[:N_TRAIN]
     y_train = y.iloc[:N_TRAIN]
     x_test = x.iloc[N_TRAIN:]
-    si.rf_impute(x_train, y_train, x_test)
+    base = si.rf_impute(x_train, y_train, x_test)
     # Try with some args.
     si.rf_impute(x_train, y_train, x_test, random_state=10, n_estimators=200)
     # Try with sample_weight_train.
     si.rf_impute(x_train, y_train, x_test, sample_weight_train=np.random.randn(N_TRAIN))
+    # Check that values are higher when using a higher mean quantile.
+    higher_q = si.rf_impute(x_train, y_train, x_test, mean_quantile=0.9)
+    assert higher_q.mean() > base.mean()
